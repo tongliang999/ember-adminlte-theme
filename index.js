93ca34ef9bcc6299a8bb53a313585ab962a83e91
kaka <huxinghai1988@gmail.com>
2016-03-11 17:04:04 +0800

    import theme css and js

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path'),
+  util = require('util');
+
 module.exports = {
-  name: 'ember-admin-lte-theme'
+  name: 'ember-adminLte-theme',
+  included: function(app){
+    
+    if (typeof app.import !== 'function' && app.app) {
+      app = app.app;
+    }
+
+    app.import('vendor/bootstrap/css/bootstrap.min.css');
+    app.import('vendor/css/AdminLTE.css');
+    app.import('vendor/css/_all-skins.min.css');
+    app.import('vendor/css/font-awesome.css');
+    app.import('vendor/bootstrap/js/bootstrap.min.js');
+    app.import('vendor/js/app.js');
+    
+    return app.toTree();
+  }
 };
