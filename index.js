e82b54668016d47c50942cf96b7d5b7479e572b2
kaka <huxinghai1988@gmail.com>
2016-03-16 12:14:34 +0800

    整理plugins 与配置加载plugins

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ var path = require('path'),
 module.exports = {
   name: 'ember-adminlte-theme',
   getExamplePath: "pub/example",
+  default_options: {
+    plugins: ["slimscroll", "fastclick"]
+  },
   treeForPublic: function(tree){
     var trees = [tree]
 
@@ -19,15 +22,22 @@ module.exports = {
     }))
 
     if(this.getEnv() == "development"){
-      trees.push(new Funnel( this.rootPath +"/vendor/", {
+      trees.push(new Funnel(this.rootPath +"/vendor/", {
         srcDir: "example",
         destDir: "example"
-      }))  
+      }))
+
+      trees.push(new Funnel(this.rootPath +"/vendor/", {
+        srcDir: "plugins",
+        destDir: "plugins"
+      }))
     }
 
     return BroccoliMergeTrees(trees, {overwrite: true });
   },
   included: function(app){
+    var self = this;
+
     if (typeof app.import !== 'function' && app.app) {
       app = app.app;
     }
@@ -35,11 +45,32 @@ module.exports = {
     this.nodeModulePath = app.options.project.nodeModulesPath
     this.rootPath = this.nodeModulePath + "/"+ this.name
 
+    this.options = this.objMerge({}, this.default_options)
+
+    if(app.options.adminLTE){
+      this.options = this.objMerge(this.options, app.options.adminLTE)
+      if(app.options.adminLTE.plugins){
+        this.options.plugins = this.getUnique(this.default_options.plugins.concat(app.options.adminLTE.plugins))
+      }
+    }
+
     app.import('vendor/bootstrap/css/bootstrap.css');
     app.import('vendor/css/AdminLTE.css');
     app.import('vendor/css/_all-skins.min.css');
     app.import('vendor/css/font-awesome.css');
     app.import('vendor/bootstrap/js/bootstrap.js');
+
+    console.log("options: ", this.options)
+    
+    this.options.plugins.forEach(function(plugin){
+      var path = self.plugins_paths[plugin]
+      if(path){
+        [].concat(path).forEach(function(path){
+          app.import("vendor/plugins/"+ path)  
+        })
+      }
+    })
+    
     app.import('vendor/js/app.js');
   },
   getEnv: function () {
@@ -47,5 +78,28 @@ module.exports = {
   },
   logger: function(message){
     console.log('['+ this.name +' HANDLING] '+ message)
+  },
+  plugins_paths: {
+    "slimscroll": "slimScroll/jquery.slimscroll.min.js",
+    "fastclick": "fastclick/fastclick.min.js",
+    "jquery": "jQuery/jQuery-2.1.4.min.js",
+    "input-mask": "jquery.inputmask.js"
+  },
+  getUnique: function(items){
+    var u = {}, a = [];
+    for(var i = 0, l = items.length; i < l; ++i){
+      if(u.hasOwnProperty(items[i])) {
+         continue;
+      }
+      a.push(items[i]);
+      u[items[i]] = 1;
+    }
+    return a;
+  },
+  objMerge: function(data, items){
+    for(var k in items){  
+      data[k] = items[k]
+    }
+    return data;
   }
 };
