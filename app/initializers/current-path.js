3dc41658ee23db55a92e0f74308096c9bead3f6d
童亮 <tongliang@zhubajie.com>
2016-06-23 09:55:00 +0800

    adminlte

--- app/initializers/current-path.js
+++ app/initializers/current-path.js
@@ -1,12 +1,6 @@
 export function initialize(app) {
-  if(app.injection){
-    app.injection("component:nav-breadcrumb", "appRoute", "route:application") 
-    app.injection("component:main-sidebar", "appRoute", "route:application") 
-  }else{
-    app.inject("component:nav-breadcrumb", "appRoute", "route:application") 
-    app.inject("component:main-sidebar", "appRoute", "route:application")   
-  }
-  
+  app.inject("component:nav-breadcrumb", "appRoute", "route:application") 
+  app.inject("component:main-sidebar", "appRoute", "route:application") 
 }
 
 export default {
